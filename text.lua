a52b2613dcfe246d7257fc22208ed9c1c28239d9
Darius Bacon <darius@wry.me>
2013-06-20 21:42:28 -0700

    OO-ify and bugfix text

--- text.lua
+++ text.lua
@@ -1,5 +1,4 @@
 -- Glossary:
---   t   text
 --   p   coordinate (position between characters in text)
 --   dir direction
 --   cs  charset
@@ -33,114 +32,116 @@
 -- say 64 bytes, instead of single characters. Or does Lua have some
 -- byte-array type?
 
-function text_make() 
-   return {head   = 0, 
-           gap    = 0,
-           length = 0,
-           marks  = {}}
-end
-
-local function clip(t, p)
-   return math.max(0, math.min(p, t.length))
-end
-
-local function clip_range(t, p, span)
-   local q = clip(t, p)
-   return q, clip(t, p + math.max(0, span)) - q
-end
-
 nowhere = -1
 
-backward = -1
-forward = 1
+backward, forward = -1, 1
 
--- Return the position in `t` before the instance of `cs` closest to
--- `p` in direction `d`.  (If there's none, return `nowhere`.)
-function text_find_char_set(t, p, dir, cs)
-   local head, gap, length = t.head, t.gap, t.length
-   p = clip(t, p)
-   if t == forward then
-      while p < head do
-         if char_set_has(cs, t[p+1]) then return p end
+function text_make() 
+   local t = {}   -- The storage for the head, gap, and tail. TODO rename?
+   local head = 0
+   local gap = 0
+   local length = 0
+
+   local function clip(p)
+      return math.max(0, math.min(p, length))
+   end
+
+   local function clip_range(p, span)
+      local q = clip(p)
+      return q, clip(p + math.max(0, span)) - q
+   end
+
+   -- Return the position before the instance of `cs` closest to `p`
+   -- in direction `d`. (If there's none, return `nowhere`.)
+   local function find_char_set(p, dir, cs)
+      p = clip(p)
+      if dir == forward then
+         while p < head do
+            if char_set_has(cs, t[p+1]) then return p end
+            p = p + 1
+         end
+         while p < length do
+            if char_set_has(cs, t[gap+p+1]) then return p end
+            p = p + 1
+         end
+      else
+         assert(dir == backward)
+         while head < p do
+            if char_set_has(cs, t[gap+p]) then return p-1 end
+            p = p - 1
+         end
+         while 0 < p do
+            if char_set_has(cs, t[p]) then return p-1 end
+            p = p - 1
+         end
+      end
+      return nowhere
+   end
+
+   -- Return the `span` characters after `p` as a string.
+   local function get(p, span)
+      p, span = clip_range(p, span)
+
+      local result = ''
+      while p < head and #result < span do
          p = p + 1
+         result = result .. t[p]
       end
-      while p < length do
-         if char_set_has(cs, t[gap+p+1]) then return p end
+      while p < length and #result < span do
          p = p + 1
+         result = result .. t[gap+p]
       end
-   else
-      assert(t == backward)
-      while head < p do
-         if char_set_has(cs, t[gap+p]) then return p-1 end
-         p = p - 1
-      end
-      while 0 < p do
-         if char_set_has(cs, t[p]) then return p-1 end
-         p = p - 1
-      end
+      return result
    end
-   return nowhere
-end
 
--- Return the `span` characters after `p` as a string.
-function text_get(t, p, span)
-   p, span = clip_range(t, p, span)
-   local head, gap, length = t.head, t.gap, t.length
-
-   local result = ''
-   while p < head and #result < span do
-      p = p + 1
-      result = result .. t[p]
-   end
-   while p < length and #result < span do
-      p = p + 1
-      result = result .. t[gap+p]
-   end
-   return result
-end
-
-local function memmove(array, dst, src, len)
-   if dst <= src then
-      for i = 0, len-1 do
-         array[dst+i] = array[src+i]
-      end
-   else
-      for i = len-1, 0, -1 do
-         array[dst+i] = array[src+i]
+   local function memmove(dst, src, len)
+      if dst <= src then
+         for i = 0, len-1 do
+            t[dst+i] = t[src+i]
+         end
+      else
+         for i = len-1, 0, -1 do
+            t[dst+i] = t[src+i]
+         end
       end
    end
-end
 
--- Replace the `span` characters after `p` by `replacement`.
-function text_replace(t, p, span, replacement)
-   p, span = clip_range(t, p, span)
-   local head, gap, length = t.head, t.gap, t.length
+   -- Replace the `span` characters after `p` by `replacement`.
+   local function replace(p, span, replacement)
+      p, span = clip_range(p, span)
 
-   -- Make position p start the tail:
-   if p <= head then
-      memmove(t, gap + p+1, p+1, head - p)
-   else
-      memmove(t, head+1, head + gap, p - head)
+      -- Make position p start the tail:
+      if p <= head then
+         memmove(gap + p+1, p+1, head - p)
+      else
+         memmove(head+1, head + gap, p - head)
+      end
+      head = p
+
+      -- Delete the next `span` characters:
+      gap = gap + span
+      length = length - span
+
+      -- Grow the array so `replacement` fits in the gap:
+      if gap < #replacement then
+         local tail, size = length - head, length + gap
+         size = size + math.floor(size/2) + #replacement
+         memmove(size - tail + 1, head + gap + 1, tail)
+         gap = size - length
+      end
+
+      -- Insert `replacement`:
+      for i = 1, #replacement do
+         t[head+i] = replacement:sub(i, i)
+      end
+      head = head + #replacement
+      length = length + #replacement
    end
-   head = p
 
-   -- Delete the next `span` characters:
-   gap = gap + span
-   length = length - span
-
-   -- Grow the array so `replacement` fits in the gap:
-   if gap < #replacement then
-      local tail, size = length - head, length + gap
-      size = size + math.floor(size/2) + #replacement
-      memmove(t, size - tail + 1, head + gap + 1, tail)
-      gap = size - length
-   end
-   t.gap = gap
-
-   -- Insert `replacement`:
-   for i = 1, #replacement do
-      t[head+i] = replacement:byte(i)
-   end
-   t.head = head + #replacement
-   t.length = length + #replacement
+   return {
+      find_char_set = find_char_set,
+      get           = get,
+      replace       = replace,
+      length        = function() return length end,
+   }
 end
