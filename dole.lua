da3512a3aef38e443fc8b5cbb7f814d0d22e171a
Darius Bacon <darius@wry.me>
2013-06-24 16:00:07 -0700

    remove redundant screen-clear

--- dole.lua
+++ dole.lua
@@ -1,10 +1,9 @@
 -- Main program
 
--- ugh to _m name
+-- ugh to _m names
 local buffer_m = require 'buffer'
 local key_m    = require 'key'
 local keymap_m = require 'keymap'
-local term_m   = require 'ansi_term'
 local stty_m   = require 'stty'
 
 local buffer = buffer_m.make()
@@ -21,7 +20,6 @@ keymap.bind('backspace', buffer.backward_delete_char)
 keymap.bind('del',       buffer.forward_delete_char)
 
 local function reacting()
-   io.write(term_m.clear_screen)
    while true do
       buffer.redisplay()
       local ch = key_m.read_key()
