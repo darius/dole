d9ed7a0e3ead3052c43aa0eb1c8818b21869224f
Darius Bacon <darius@wry.me>
2013-06-24 16:23:24 -0700

    rename keyboard module

--- dole.lua
+++ dole.lua
@@ -1,19 +1,17 @@
 -- Main program
 
--- ugh to _m names
-local buffer_m = require 'buffer'
-local key_m    = require 'key'
-local keymap_m = require 'keymap'
-local stty_m   = require 'stty'
-
-local buffer = buffer_m.make()
+local buffer_m   = require 'buffer'
+local keyboard_m = require 'keyboard'
+local keymap_m   = require 'keymap'
+local stty_m     = require 'stty'
 
 local C = keymap_m.ctrl
+
+local buffer = buffer_m.make()
 
 buffer.keymap.bind(C('B'),      function() buffer.move_char(-1) end)
 buffer.keymap.bind(C('F'),      function() buffer.move_char(1) end)
 buffer.keymap.bind(C('Q'),      'exit')
-
 buffer.keymap.bind('\r',        function() buffer.insert('\n') end)
 buffer.keymap.bind('backspace', buffer.backward_delete_char)
 buffer.keymap.bind('del',       buffer.forward_delete_char)
@@ -21,7 +19,7 @@ buffer.keymap.bind('del',       buffer.forward_delete_char)
 local function reacting()
    while true do
       buffer.redisplay()
-      local ch = key_m.read_key()
+      local ch = keyboard_m.read_key()
       local command = buffer.keymap.get(ch)
       if command == 'exit' then break end
       command(ch)
